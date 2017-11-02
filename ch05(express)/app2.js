1fd366c707dc762d2e46ac3146ee08faf1752724
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-11-03 00:08:47 +0300

    6th Commit

---
+++ ch05(express)/app2.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var http = require('http');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next){
+    console.log('First Middleware is called.');
+    
+    res.writeHead(200, {"Content-Type": "text/html; charset=utf8"});
+    res.end('<h1>Result from server.</h1>');
+});
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express Web Server : ' + app.get('port'));
+});
