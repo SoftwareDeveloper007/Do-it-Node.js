0af24a3edad8e165763fd026edfc5eeda1a5756e
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-11-02 22:40:42 +0300

    2nd Commit

--- ch01/test9.js
+++ ch01/test9.js
@@ -1,2 +1,10 @@
 var users = [{name: 'Jack', age: 30}, {name: 'John', age: 29}];
 
+var oper = function(a, b){
+    return a + b;
+};
+
+users.push(oper);
+
+console.dir(users);
+console.log('세번째 배렬 요소로 함수로 실행 : ' + users[2](10,10));
