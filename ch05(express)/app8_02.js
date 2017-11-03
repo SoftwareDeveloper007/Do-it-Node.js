1adaf24c08a512f583875e236241c0782184707e
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-11-03 18:28:25 +0300

    11th Commit

---
+++ ch05(express)/app8_02.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var http = require('http');
+var static = require('serve-static');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use('/public', static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+router.route('/process/login/:name').post(function(req, res){
+    console.log('/process/login/:name routing function'); 
+    
+    var paramName = req.params.name;
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+    res.writeHead(200, {"Content-Type": "text/html; charset=utf8"});
+    res.write("<h1>Login response from server</h1>");
+    res.write("<div><p>" + paramName + "</p></div>");
+    res.write("<div><p>" + paramId + "</p></div>");
+    res.write("<div><p>" + paramPassword + "</p></div>")
+    res.end();
+});
+
+app.all('*', function(req, res){
+    res.status(404).send('<h1>404 Not Found</h1>');
+});
+
+app.use('/', router);
+
+
+
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express Web Server : ' + app.get('port'));
+});
