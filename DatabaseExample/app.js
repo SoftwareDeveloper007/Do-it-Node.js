53d250955cd8fa478d589676f3b3575263d93d0c
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-11-05 10:26:57 +0300

    19th Commit

--- DatabaseExample/app.js
+++ DatabaseExample/app.js
@@ -6,10 +6,10 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
 
-// 에러 핸들러 모듈 사용
+// Error Handler Module
 var expressErrorHandler = require('express-error-handler');
 
-// mongodb 모듈 사용
+// mongodb Module
 var MongoClient = require('mongodb').MongoClient;
 
 var database;
@@ -19,11 +19,11 @@ function connectDB() {
 
     MongoClient.connect(databaseUrl, function (err, db) {
         if (err){
-            console.log('데이터베이스 련결시 에러 발생함.');
+            console.log('Error happened when connecting to database.');
             return;
         }
 
-        console.log('데이터베이스에 련결됨 : ' + databaseUrl);
+        console.log('Connected to database : ' + databaseUrl);
         database = db;
     });
 }
@@ -45,7 +45,62 @@ app.use(expressSession({
 
 var router = express.Router();
 
+router.route('/process/login').post(function (req, res) {
+    console.log('/process/login : Routing function is called');
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+    console.log("Request parameter : + " + paramId + ', ' + paramPassword);
+
+    if(database){
+        authUser(database, paramId, paramPassword, function (err, docs) {
+            if(err){
+                console.log("Error happened.");
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf8"});
+                res.write('<h1>Error happened</h1>');
+                res.end();
+                return;
+            }
+
+            if(docs){
+                console.dir(docs);
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf8"});
+                res.write('<h1>User login is succeeded</h1>');
+                res.write('<div><p>User : ' + docs[0].name + '</p></div>')
+                res.write('<br><br><a href="/public/login.html">Log in again</a> ');
+                res.end();
+                return;
+            }
+            else {
+                console.log("Error happened.");
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf8"});
+                res.write("<h1>Can't not visit user database</h1>");
+                res.end();
+                return;
+            }
+        })
+    }
+});
+
 app.use('/', router);
+
+var authUser = function (db, id, password, callback) {
+    console.log('authUser is called :' + id + ', ' + password);
+    var users = db.collection('users');
+    users.find({"id": id, "password": password}).toArray(function (err, docs) {
+        if(err){
+            callback(err, null);
+            return;
+        }
+        if(docs.length > 0){
+            console.log("Identified User is found");
+            callback(null, docs);
+        }
+        else {
+            console.log("Identified User is not found.");
+            callback(null, null);
+        }
+    });
+};
 
 // 404 Error page
 var errorHandler = expressErrorHandler({
