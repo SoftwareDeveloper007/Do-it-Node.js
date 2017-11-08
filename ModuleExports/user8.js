1a3b8a03a4a8109c7164b72791118d0a72649eaa
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-11-08 21:45:31 +0300

    47th Commit

---
+++ ModuleExports/user8.js
@@ -0,0 +1,16 @@
+function User(id, name) {
+    this.id = id;
+    this.name = name;
+}
+
+User.prototype.getUser = function () {
+    return {id: this.id, name: this.name};
+};
+
+User.prototype.group = {id: 'group01', name: 'Friends'};
+
+User.prototype.printUser = function () {
+    console.log('user name : ' + this.name + ', group : ' + this.group.name);
+};
+
+module.exports = new User('test01', 'Jack');
